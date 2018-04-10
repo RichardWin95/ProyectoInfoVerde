ecfa9d68bcca492db64625c487a0d9eae708a3c5
richiArt95 <richard.andres04@gmail.com>
2018-04-09 23:07:12 -0500

    Se corrigieron archivos perdidos

--- HuellasVerdes/config.py
+++
@@ -1,6 +0,0 @@
-class Config(object):
-    SECRET_KEY = '1234'
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://richard:12345@localhost/CO2'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
